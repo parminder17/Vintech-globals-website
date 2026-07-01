4fa6de79a462323d0e3781efb9c05c26a79509ef
Parminder Singh <145452825+parminder17@users.noreply.github.com>
2026-07-01 18:08:13 +0530

    Update entry-client.tsx

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,5 +1,12 @@
 import { hydrateRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 1. Router import karo
 import App from './App';
 import './index.css';
 
-hydrateRoot(document.getElementById('root')!, <App />);
+// 2. App nu <BrowserRouter> naal wrap karo
+hydrateRoot(
+  document.getElementById('root')!, 
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
